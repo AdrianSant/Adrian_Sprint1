6c2779b8509922ab08ce5160d084e98f5af7d73f
Adrian <Adrian@DESKTOP-KBJT4MT>
2022-03-22 17:16:34 -0400

    Add project files.

---
+++ GroupProjectCST4708/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("Login Successful")
+        Dim homePage As Home
+        homePage = New Home
+        homePage.Show()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim registerPage As Register
+        registerPage = New Register
+        registerPage.Show()
+    End Sub
+End Class
