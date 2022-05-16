c940effaa7fa54fb55762e899f9c888bc1b63a5c
Adrian Santos <Adrian.Santos@mail.citytech.cuny.edu>
2022-05-16 01:47:58 -0400

    removes pictures from resources

--- GroupProjectCST4708/My Project/Application.Designer.vb
+++ GroupProjectCST4708/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Login
+            Me.MainForm = Global.GroupProjectCST4708.Form1
         End Sub
     End Class
 End Namespace
